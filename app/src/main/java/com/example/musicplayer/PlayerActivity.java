e696f6748845f4689cb764a93840623ab96b769e
Vinicios <viniciuslins61@gmail.com>
2024-05-26 13:49:21 -0300

    Modificações

---
+++ app/src/main/java/com/example/musicplayer/PlayerActivity.java
@@ -0,0 +1,28 @@
+package com.example.musicplayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.musicplayer.databinding.ActivityPlayerBinding;
+
+public class PlayerActivity extends AppCompatActivity {
+ActivityPlayerBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityPlayerBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
